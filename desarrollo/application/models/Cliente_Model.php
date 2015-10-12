6a4252fdee4cefff8b6c13d3d026dd60715d77d4
llanes villagra <milciades.anals@msn.com>
2015-10-12 15:57:21 -0300

    Desarrollo modulo clientes.

---
+++ desarrollo/application/models/Cliente_Model.php
@@ -0,0 +1,113 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Cliente_Model extends CI_Model {
+	var $table = 'cliente';
+	var $user = 'usuario';
+	var $where = 'usuario.Empleado_idEmpleado = cliente.idCliente and usuario.Permiso_idPermiso = 2';
+	var $column = array('Nombres','Apellidos','Direccion','Telefono','Email');
+	var $order = array('idCliente' => 'desc');
+
+	public function __construct()
+	{
+		parent::__construct();
+		//Haga su magia aquí
+		$this->load->database();
+	}
+	private function _get_datatables_query()
+	{
+		$this->db->from($this->table);
+		$this->db->from($this->user);
+		$i = 0;
+
+		foreach ($this->column as $item)
+		{
+			$this->db->where($this->where);
+			if($_POST['search']['value'])
+				($i===0) ? $this->db->like($item, $_POST['search']['value']) : $this->db->or_like($item, $_POST['search']['value']);
+			$column[$i] = $item;
+			$i++;
+		}
+
+		if(isset($_POST['order']))
+		{
+			$this->db->order_by($column[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
+		}
+		else if(isset($this->order))
+		{
+			$order = $this->order;
+			$this->db->order_by(key($order), $order[key($order)]);
+		}
+	}
+
+	function get_cliente()
+	{
+		$this->_get_datatables_query();
+		if($_POST['length'] != -1)
+		$this->db->where($this->where);
+		$this->db->limit($_POST['length'], $_POST['start']);
+		$query = $this->db->get();
+		return $query->result();
+	}
+
+	function count_filtro()
+	{
+		$this->_get_datatables_query();
+		$query = $this->db->get();
+		return $query->num_rows();
+	}
+
+	public function count_todas()
+	{
+		$this->db->from($this->table);
+		$this->db->from($this->user);
+		$this->db->where($this->where);
+		return $this->db->count_all_results();
+	}
+
+	public function get_by_id($id)
+	{
+		$this->db->from($this->table);
+		$this->db->from($this->user);
+		$this->db->where($this->where);
+		$this->db->where('idCliente',$id);
+		$query = $this->db->get();
+
+		return $query->row();
+	}
+
+	public function save($data)
+	{
+		$this->db->insert($this->table, $data);
+		return $this->db->insert_id();
+	}
+	public function save2($data)
+	{
+		$this->db->insert($this->user, $data);
+		return $this->db->insert_id();
+	}
+
+	public function update($where, $data)
+	{
+		$this->db->update($this->table, $data, $where);
+		return $this->db->affected_rows();
+	}
+	public function update2($idCliente, $data)
+	{	
+		$this->db->where('Empleado_idEmpleado', $idCliente);
+		$this->db->where('Permiso_idPermiso = 2');
+		$this->db->update($this->user, $data);
+		return $this->db->affected_rows();
+	}
+
+	public function delete_by_id($idCliente)
+	{
+		$this->db->where('idCliente', $idCliente);
+		$this->db->delete($this->table);
+		$this->db->where('usuario.Empleado_idEmpleado',$idCliente);
+		$this->db->delete($this->user);
+	}
+
+}
+
+/* End of file marca_Model.php */
+/* Location: ./application/models/marca_Model.php */
---
+++ desarrollo/application/views/Cliente/script.php
@@ -0,0 +1,166 @@
+  <script type="text/javascript">
+
+    var save_method; //for save method string
+    var table;
+    $(document).ready(function() {
+      table = $('#table').DataTable({ 
+        
+        "processing": true, //Feature control the processing indicator.
+        "serverSide": true, //Feature control DataTables' server-side processing mode.
+        
+        // Load data for the table's content from an Ajax source
+        "ajax": {
+            "url": "<?php echo site_url('index.php/Cliente/cliente/ajax_list')?>",
+            "type": "POST"
+        },
+
+        //Set column definition initialisation properties.
+        "columnDefs": [
+        { 
+          "targets": [ -1 ], //last column
+          "orderable": false, //set not orderable
+        },
+        ],
+
+      });
+    });
+
+    function add_cliente()
+    {
+     $(".N,.A,.D,.T,.E,.U,.P,.PF").html("").css({"display":"none"});
+      save_method = 'add';
+      $('#form')[0].reset(); // reset form on modals
+      $('#modal_form').modal('show'); // show bootstrap modal
+      $('.modal-title').text('Agregar Cliente'); // Set Title to Bootstrap modal title
+    }
+
+    function edit_person(idCliente)
+    {
+      $(".N,.A,.D,.T,.E,.U,.P,.PF").html("").css({"display":"none"});
+      save_method = 'update';
+      // $('#PF').hide();
+      $('#form')[0].reset(); // reset form on modals
+
+      //Ajax Load data from ajax
+      $.ajax({
+        url : "<?php echo site_url('index.php/Cliente/cliente/ajax_edit/')?>/" + idCliente,
+        type: "GET",
+        dataType: "JSON",
+        success: function(data)
+        {
+           
+            $('[name="idCliente"]').val(data.idCliente);
+            $('[name="Nombres"]').val(data.Nombres);
+            $('[name="Apellidos"]').val(data.Apellidos);
+            $('[name="Direccion"]').val(data.Direccion);
+            $('[name="Telefono"]').val(data.Telefono);
+            $('[name="Email"]').val(data.Email);
+             $('[name="usuario"]').val(data.Usuario);
+              $('[name="password"]').val(data.Password);
+                $('[name="passconf"]').val(data.Password);
+            
+            $('#modal_form').modal('show'); // show bootstrap modal when complete loaded
+            $('.modal-title').text('Editar Cliente'); // Set title to Bootstrap modal title
+            
+        },
+        error: function (jqXHR, textStatus, errorThrown)
+        {
+            alert('Error al obtener datos');
+        }
+    });
+    }
+
+    function reload_table()
+    {
+      table.ajax.reload(null,false); //reload datatable ajax 
+    }
+    $(function() {
+    $('#form').submit(function(e) {
+      var url;
+      if(save_method == 'add') 
+      {
+        url = "<?php echo site_url('index.php/Cliente/cliente/ajax_add')?>";
+      }
+      else
+      {
+        url = "<?php echo site_url('index.php/Cliente/cliente/ajax_update')?>";
+      }
+           $.ajax({
+                      type : 'POST',
+                      url : url, // octengo la url del formulario
+                      data: $(this).serialize(), // serilizo el formulario
+                      success : function(data) {
+                         var json = JSON.parse(data);// parseo la dada devuelta por json
+                          $(".N,.A,.D,.T,.E,.U,.P,.PF").html("").css({"display":"none"});
+                          if (json.res == "error") {
+                            if (json.Nombres) {
+                               $(".N").append(json.Nombres).css({"display":"block"}); // mostrar validation de iten usuario
+                            }
+                            if (json.Apellidos) {
+                               $(".A").append(json.Apellidos).css({"display":"block"}); // mostrar validation de iten usuario
+                            }
+                            if (json.Direccion) {
+                               $(".D").append(json.Direccion).css({"display":"block"}); // mostrar validation de iten usuario
+                            }
+                            if (json.Telefono) {
+                               $(".T").append(json.Telefono).css({"display":"block"}); // mostrar validation de iten usuario
+                            }
+                            if (json.Email) {
+                               $(".E").append(json.Email).css({"display":"block"}); // mostrar validation de iten usuario
+                            }
+                            if (json.usuario) {
+                               $(".U").append(json.usuario).css({"display":"block"}); // mostrar validation de iten usuario
+                            }
+                            if (json.password) {
+                               $(".P").append(json.password).css({"display":"block"}); /// mostar validation  de iten pass
+                            }
+                           if (json.passconf) {
+                               $(".PF").append(json.passconf).css({"display":"block"}); /// mostar validation  de iten pass
+                            }
+                          }else{ // si pasa la validation redireccionar al ligin del control de acceso
+                              // var url = "<?= site_url('index.php/Login/login')?>";
+                              // $(location).attr('href',url);
+                              // setTimeout($(location).attr('href',url), 10000); 
+                              // $("#for_login-registro").hide();
+                              // $("#mensaje").show();
+                             // setTimeout('document.location.reload()',500);
+                                         $('#modal_form').modal('hide');
+                                          reload_table();
+                           }
+                     },
+                      // código a ejecutar si la petición falla;
+                      error : function(xhr, status) {
+                          alert('Disculpe, existió un problema');
+                          console.log('error(jqXHR, textStatus, errorThrown)');
+                      },
+                  });
+      e.preventDefault();
+    })
+    });
+
+
+    function delete_person(id)
+    {
+      if(confirm('¿Seguro borrar estos datos?'))
+      {
+        // ajax delete data to database
+          $.ajax({
+            url : "<?php echo site_url('index.php/Cliente/cliente/ajax_delete')?>/"+id,
+            type: "POST",
+            dataType: "JSON",
+            success: function(data)
+            {
+               //if success reload ajax table
+               $('#modal_form').modal('hide');
+               reload_table();
+            },
+            error: function (jqXHR, textStatus, errorThrown)
+            {
+                alert('Error al intentar borrar');
+            }
+        });
+         
+      }
+    }
+
+
