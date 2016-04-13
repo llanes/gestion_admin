6a3bb8d2d6575f58a3396d393221b6ea22ee9788
llanes villagra <milciades.anals@msn.com>
2016-04-12 21:36:04 -0400

    conflictos

--- desarrollo/application/views/Presupuesto_arquiler/footer.php
+++
@@ -1,8 +0,0 @@
-                                  </div>
-                                  <div id="listados_presupuesto" class="tab-pane"></div>
-                                  <div id="listados_alquiler" class="tab-pane"></div>
-                               </div>
-                          </div>
-   </section>
-  <!-- fin <div class="content-wrapper"> -->
-</div>
